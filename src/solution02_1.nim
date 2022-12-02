3947af2408f72ec151a8d3342a4771f30d0a46a1
Mike Schwörer <pubgit@mikescher.com>
2022-12-02 12:23:56 +0100

    Day 02 - Problem 1

--- src/solution02_1.nim
+++ src/solution02_1.nim
@@ -1,8 +1,59 @@
+import std/strutils
 
+proc get_rps_winner(m1: int, m2: int): int =
+    if m1 == 0 and m2 == 0:
+        return 0
+    if m1 == 0 and m2 == 1:
+        return 2
+    if m1 == 0 and m2 == 2:
+        return 1
+
+    if m1 == 1 and m2 == 0:
+        return 1
+    if m1 == 1 and m2 == 1:
+        return 0
+    if m1 == 1 and m2 == 2:
+        return 2
+
+    if m1 == 2 and m2 == 0:
+        return 2
+    if m1 == 2 and m2 == 1:
+        return 1
+    if m1 == 2 and m2 == 2:
+        return 0
+
+    return -1
+    
 proc run02_1(): string =
     const input = staticRead"../input/day02.txt"
 
-    return "TODO"
+    var score = 0
+
+    for line in splitLines(input):
+        if line == "":
+            continue
+
+        let move_elve = int(line[0]) - int('A')
+        let move_me   = int(line[2]) - int('X')
+
+        let delta1 = (move_me + 1)
+        var delta2 = 0
+
+        let winner = get_rps_winner(move_elve, move_me)
+
+        if winner == 0:
+            delta2 = 3 # draw
+        elif winner == 1:
+            delta2 = 0 # elve win
+        elif winner == 2:
+            delta2 = 6 # me win
+
+        # echo line , "  ->  " , move_elve, "|", move_me, " -> ", winner, " || ", score, " + ", delta1, " + ", delta2
+
+        score += delta1
+        score += delta2
+
+    return intToStr(score)
 
 when not defined(js):
     echo run02_1()
