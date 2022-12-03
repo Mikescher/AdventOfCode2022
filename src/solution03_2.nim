2cd72dc1207442f5709c868893e08a5cf48b2ee0
Mike Schwörer <pubgit@mikescher.com>
2022-12-03 13:41:26 +0100

    Day 03 - Problem 2

--- src/solution03_2.nim
+++ src/solution03_2.nim
@@ -1,8 +1,29 @@
+import std/strutils
+import std/sequtils
 
 proc run03_2(): string =
     const input = staticRead"../input/day03.txt"
 
-    return "TODO"
+    var res = 0
+
+    let lines = splitLines(input).filter(proc(p: string): bool = p != "")
+
+    for lineSet in lines.distribute(int(lines.len/3)):
+        
+        let line1 = lineSet[0]
+        let line2 = lineSet[1]
+        let line3 = lineSet[2]
+
+        let badge = line1.toSeq().filter(proc(p: char): bool = line2.contains(p) and line3.contains(p))[0]
+
+        let prio = if int(badge) >= int('a'): int(badge) - int('a') + 1 else: int(badge) - int('A') + 27
+        
+        # echo badge, " : ", prio
+
+        res += prio
+
+    return intToStr(res)
+
 
 when not defined(js):
     echo run03_2()
