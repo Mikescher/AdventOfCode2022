07ec0721633df9c3f8fb8fa6fa7069ca8547753a
Mike Schwörer <pubgit@mikescher.com>
2022-12-06 08:16:33 +0100

    Day 06 - Problem 1

--- src/solution06_1.nim
+++ src/solution06_1.nim
@@ -1,8 +1,19 @@
+import std/strutils
+import std/sequtils
+import std/algorithm
 
 proc run06_1(): string =
     const input = staticRead"../input/day06.txt"
 
-    return "TODO"
+    let chars = input.toSeq()
+
+    let idarr = (0 .. len(chars) - 4 - 1).toSeq().filter(proc(it: int):bool = chars[it .. it+3].deduplicate().len() == 4 )
+
+    #echo ""
+    #echo idarr
+    #echo ""
+
+    return intToStr(idarr[0] + 4)
 
 
 when not defined(js):
